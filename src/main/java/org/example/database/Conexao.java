5fc43dfc273c767d743f4b166ac6839abda4369f
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-02-06 19:41:42 -0300

    feat: finaliza cocnexao banco

--- src/main/java/org/example/database/Conexao.java
+++ src/main/java/org/example/database/Conexao.java
@@ -1,4 +1,15 @@
 package org.example.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/provami78?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWD = "mysqlPW";
+
+    public static Connection conexao() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWD);
+    }
 }
